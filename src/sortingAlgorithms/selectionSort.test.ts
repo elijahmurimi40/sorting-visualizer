1f4a62a0f7be30b0f4c10ca64b0a42b8aa1f8384
Fortie40 <elijahmurimi40@gmail.com>
2020-12-30 11:29:40 +0300

    fix selection sort test

--- src/sortingAlgorithms/selectionSort.test.ts
+++ src/sortingAlgorithms/selectionSort.test.ts
@@ -2,14 +2,21 @@ import selectionSort from './selectionSort';
 import generateRandomArray from './generateRandomArray';
 import { Bar } from '../helperFunctions/ArrayBars';
 
-const array = generateRandomArray();
-const jsSortedArray = array.slice().sort((a: Bar, b: Bar) => a.value - b.value);
-const selectionSortSortedArray = selectionSort(array, null, null, null, () => { }, () => { });
+for (let i = 0; i < 100; i += 1) {
+  const array = generateRandomArray();
+  const jsSortedArray = array.slice().sort((a: Bar, b: Bar) => a.value - b.value);
+  const selectionSortSortedArray = selectionSort(array, null, null, null, () => { }, () => { });
 
-test('Selection Sort Test', () => {
-  selectionSortSortedArray.then((arr) => {
-    expect(JSON.stringify(arr)).toEqual(JSON.stringify(jsSortedArray));
-  })
-    // eslint-disable-next-line no-console
-    .catch((err) => { console.log(err); });
-});
+  test('Selection Sort Test', () => {
+    selectionSortSortedArray.then((arr) => {
+      arr.map((val: Bar) => {
+        // eslint-disable-next-line no-param-reassign
+        val.idx = 0;
+        return arr;
+      });
+      expect(JSON.stringify(arr)).toEqual(JSON.stringify(jsSortedArray));
+    })
+      // eslint-disable-next-line no-console
+      .catch((err) => { console.log(err); });
+  });
+}
